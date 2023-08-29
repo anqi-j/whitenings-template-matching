64bce71cbd0e434d8be0af50045caff502af7944
Anqi Zhang <anqikylin@gmail.com>
2023-08-29 09:36:06 -0500

    Reorganize data for easier access.

--- experiment/Exp2_3cpdTargets/AZ2/deconstruct.m
+++
@@ -1,7 +0,0 @@
-subject = 'AZ2';
-load(['Stimulus_',subject,'.mat']);
-load(['Results_',subject,'.mat']);
-load(['Experiment_',subject,'.mat']);
-save(['vars_Stimulus_',subject,'.mat'], '-struct', 'Stimulus');
-save(['vars_Results_',subject,'.mat'], '-struct', 'Results');
-save(['vars_Experiments_',subject,'.mat'], '-struct', 'Experiment');
